bbd9e1b84ed9f73240c0256cc37d62231ce01385
dempseyt <dempsey.thompson@journeyone.com.au>
2024-07-23 15:53:47 +0800

    GREEN: login given a user already exists and they provide the correct credentials, they are provided with a session token

--- src/user/user-router.ts
+++ src/user/user-router.ts
@@ -1,12 +1,41 @@
+import { JwtPublicKey, KeySet } from "@/global";
 import validateUserRegisterRequestBody from "@/validate-user-register-request-body";
-import express from "express";
+import express, { RequestHandler } from "express";
+import { EncryptJWT } from "jose";
 import { UserServiceInterface } from "./user-service";
 
 const loginRequestHandlerFactory =
-  (userService: UserServiceInterface) => (req, res, next) => {};
+  (
+    userService: UserServiceInterface,
+    jwtPublicKey?: JwtPublicKey
+  ): RequestHandler =>
+  async (req, res, next) => {
+    // authenticate the request
+    // issue a token
+    // send the token with the response
+    const email = req.body.userName;
+    const jwt = await new EncryptJWT({
+      username: email,
+      roles: [],
+    })
+      .setProtectedHeader({
+        alg: "RSA-OAEP-256",
+        typ: "JWT",
+        enc: "A256GCM",
+      })
+      .setIssuer("connect4-http-server")
+      .setIssuedAt()
+      .setExpirationTime("1 day from now")
+      .setNotBefore("0 sec from now")
+      .setSubject(email)
+      .encrypt(jwtPublicKey);
+    res.appendHeader("authorization", jwt).send();
+    next();
+  };
 
 const registerRequestHandlerFactory =
-  (userService: UserServiceInterface) => (req, res, next) => {
+  (userService: UserServiceInterface): RequestHandler =>
+  (req, res, next) => {
     const { isValid, errors } = validateUserRegisterRequestBody(req.body);
     if (!isValid) {
       res.status(403).send({ errors });
@@ -21,11 +50,17 @@ const registerRequestHandlerFactory =
       .catch(next);
   };
 
-const userRouterFactory = (userService: UserServiceInterface) => {
+const userRouterFactory = (
+  userService: UserServiceInterface,
+  keySet: KeySet
+) => {
   const userRouter = express.Router();
 
   userRouter.post("/register", registerRequestHandlerFactory(userService));
-  userRouter.post("/login", loginRequestHandlerFactory(userService));
+  userRouter.post(
+    "/login",
+    loginRequestHandlerFactory(userService, keySet?.jwtPublicKey)
+  );
 
   return userRouter;
 };
