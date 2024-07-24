5bade16146ac70e14fdb18122133e843d73ddb36
dempseyt <dempsey.thompson@journeyone.com.au>
2024-07-24 16:30:21 +0800

    RED: given a token which is expired it returns false

--- src/get-is-user-authorised.spec.ts
+++ src/get-is-user-authorised.spec.ts
@@ -1,4 +1,4 @@
-import { generateKeyPair } from "jose";
+import { EncryptJWT, generateKeyPair } from "jose";
 import getIsUserAuthorised from "./get-is-user-authorised";
 
 describe("get-is-user-authorised", () => {
@@ -7,6 +7,16 @@ describe("get-is-user-authorised", () => {
       it("returns false", async () => {
         const { privateKey } = await generateKeyPair("RS256");
         const token = "fgojkhuiogutgh";
+        expect(getIsUserAuthorised(token, privateKey)).resolves.toBe(false);
+      });
+    });
+    describe("which is expired", () => {
+      it("returns false", async () => {
+        const { privateKey, publicKey } = await generateKeyPair("RS256");
+        const token = await new EncryptJWT()
+          .setProtectedHeader({ alg: "RSA-OAEP-256", enc: "A128CBC-Hs256" })
+          .setExpirationTime("1 day ago")
+          .encrypt(publicKey);
         expect(getIsUserAuthorised(token, privateKey)).resolves.toBe(false);
       });
     });
