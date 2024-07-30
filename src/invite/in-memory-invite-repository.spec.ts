b93d3a3e36923f2fa136c87fc5300ffbadf0c3de
dempseyt <dempsey.thompson@journeyone.com.au>
2024-07-30 14:45:31 +0800

    RED: given defaults of an invite it creates an invite

--- src/invite/in-memory-invite-repository.spec.ts
+++ src/invite/in-memory-invite-repository.spec.ts
@@ -1,10 +1,20 @@
+import InMemoryInviteRepository from "./in-memory-invite-repository";
+
+const lengthOfDayInMilliseconds = 1000 * 60 * 60 * 24;
+
 describe("in-memory-invite-repository", () => {
   describe("given defaults of an invite", () => {
     it("creates the invite", async () => {
+      jest.useFakeTimers({ doNotFake: ["setImmediate"] });
+      const currentTime = Date.now();
+      jest.setSystemTime(currentTime);
+
       const inMemoryInviteRepository = new InMemoryInviteRepository();
+      const expiration = currentTime + lengthOfDayInMilliseconds;
       const inviteDetails = {
         inviter: "john@mail.com",
         invitee: "gerald@mail.com",
+        exp: expiration,
       };
       const createdInvite = await inMemoryInviteRepository.create(
         inviteDetails
@@ -13,8 +23,12 @@ describe("in-memory-invite-repository", () => {
       expect(createdInvite).toEqual({
         inviter: "john@mail.com",
         invitee: "gerald@mail.com",
+        exp: expiration,
+        // @ts-ignore
         uuid: expect.toBeUuid(),
+        status: "PENDING",
       });
+      jest.useRealTimers();
     });
   });
 });
