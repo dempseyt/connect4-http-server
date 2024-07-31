9cc095e4dc5140ea6b3785760c2a7ffc6a0f2132
dempseyt <dempsey.thompson@journeyone.com.au>
2024-07-31 16:31:22 +0800

    REFACTOR: given the inviter is not logged in when the inviter sends an invitation it returns http status code 401

--- src/invite/invite-router-factory.ts
+++ src/invite/invite-router-factory.ts
@@ -1,5 +1,13 @@
 import express, { RequestHandler } from "express";
 import InviteService from "./invite-service";
+
+const authorisationMiddleware: RequestHandler = (req, res, next) =>
+  res.locals.claims?.email
+    ? next()
+    : res.status(401).send({
+        errors: ["You must be logged in to send an invite"],
+      });
+
 const createInviteRequestHandlerFactory =
   (inviteService: InviteService): RequestHandler =>
   (req, res, next) => {
@@ -12,10 +20,13 @@ const createInviteRequestHandlerFactory =
       exp,
     };
     res.status(201).send({ invite: invitationDetails });
+
+    next();
   };
 
 const inviteRouterFactory = (inviteService: InviteService) => {
   const inviteRouter = express.Router();
+  inviteRouter.use(authorisationMiddleware);
   inviteRouter.post("/", createInviteRequestHandlerFactory(inviteService));
   return inviteRouter;
 };
