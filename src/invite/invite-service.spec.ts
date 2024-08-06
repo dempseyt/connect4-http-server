ad57ff02456f86e548bcdf0df504553bc65a2c53
dempseyt <dempsey.thompson@journeyone.com.au>
2024-08-06 15:32:52 +0800

    RED: invite-service › given an inviter who is an existing user › and an invitee who is an existing user › and the inviter and invitee are the same user › throws an 'Invalid invitation' error

--- src/invite/invite-service.spec.ts
+++ src/invite/invite-service.spec.ts
@@ -1,20 +1,19 @@
 import InMemoryUserRepository from "../user/in-memory-user-repository";
 import UserService from "../user/user-service";
 import InMemoryInviteRepository from "./in-memory-invite-repository";
-import InviteService from "./invite-service";
+import InviteService, { InvalidInvitationError } from "./invite-service";
 
 describe("invite-service", () => {
+  const userRepository = new InMemoryUserRepository();
+  const userService = new UserService(userRepository);
+  const inviteRepository = new InMemoryInviteRepository();
+  const inviteService = new InviteService(userService, inviteRepository);
   describe("given an inviter who is an existing user", () => {
     describe("and an invitee who is an existing user", () => {
       it("creates an invite", async () => {
         jest.useFakeTimers({ doNotFake: ["setImmediate"] });
         const currentTime = Date.now();
         jest.setSystemTime(currentTime);
-        const userRepository = new InMemoryUserRepository();
-        const userService = new UserService(userRepository);
-        const inviteRepository = new InMemoryInviteRepository();
-        const inviteService = new InviteService(userService, inviteRepository);
-
         const inviteDetails = inviteService.create({
           inviter: "john@mail.com",
           invitee: "gerald@mail.com",
@@ -29,6 +28,19 @@ describe("invite-service", () => {
           status: "PENDING",
         });
         jest.useRealTimers();
+      });
+      describe("and the inviter and invitee are the same user", () => {
+        it("throws an 'Invalid invitation' error", () => {
+          const invitationCreationDetails = {
+            inviter: "john@mail.com",
+            invitee: "john@mail.com",
+          };
+          expect(inviteService.create).rejects.toThrow(
+            new InvalidInvitationError(
+              "Users cannot send invites to themselves"
+            )
+          );
+        });
       });
     });
   });
