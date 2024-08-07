76f94fbd8ea18f7f39ad344d0bcd3da749ac78fe
dempseyt <dempsey.thompson@journeyone.com.au>
2024-08-07 14:52:23 +0800

    RED: in-memory-invite-repository › given an existing user email › retrieves the invites for that user

--- src/invite/in-memory-invite-repository.spec.ts
+++ src/invite/in-memory-invite-repository.spec.ts
@@ -1,4 +1,5 @@
 import InMemoryInviteRepository from "./in-memory-invite-repository";
+import { InviteStatus } from "./invite-service-types.d";
 
 const lengthOfDayInMilliseconds = 1000 * 60 * 60 * 24;
 
@@ -28,6 +29,25 @@ describe("in-memory-invite-repository", () => {
         uuid: expect.toBeUuid(),
         status: "PENDING",
       });
+      jest.useRealTimers();
+    });
+  });
+  describe("given an existing user email", () => {
+    it("retrieves the invites for that user", () => {
+      jest.useFakeTimers();
+      const currentTime = Date.now();
+      jest.setSystemTime();
+      const inviteRepository = new InMemoryInviteRepository();
+      expect(inviteRepository.findInvites("gerald@mail.com")).toEqual([
+        {
+          // @ts-ignore
+          uuid: expect.toBeUuid(),
+          inviter: "john@mail.com",
+          invitee: "gerald@mail.com",
+          exp: currentTime + lengthOfDayInMilliseconds,
+          status: InviteStatus.PENDING,
+        },
+      ]);
       jest.useRealTimers();
     });
   });
